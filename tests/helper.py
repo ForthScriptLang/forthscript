f4bd61445921eb528e2d748df642f4e1e2bf8bd1
Alexey <39865189+ugruk@users.noreply.github.com>
2020-08-23 18:17:29 +0300

    new structure of simpleCases

--- tests/helper.py
+++ tests/helper.py
@@ -1,6 +1,9 @@
 import json
 import glob
 
+complexCasesPath = "cases/*"
+simpleCasesPath = "simpleCases/*"
+
 def create_tests(func_name, file):
     def decorator(cls):
         func = getattr(cls, func_name)
@@ -23,7 +26,7 @@ def cases_from_files(func_name):
         func = getattr(cls, func_name)
         # Set new funcs to class:
         # print("cases: ", glob.glob("cases/*"))
-        casesFolders = glob.glob("cases/*")
+        casesFolders = glob.glob(complexCasesPath)
         for i, params in enumerate(casesFolders):
             def tmp(params=params):  # need for http://stackoverflow.com/q/7546285/1113207
                 def wrapper(self, *args, **kwargs):
@@ -32,5 +35,24 @@ def cases_from_files(func_name):
             setattr(cls, func_name + '_' + str(params.replace('\\','/').split('/')[1]), tmp())
         # Remove func from class:
         setattr(cls, func_name, None)
+        return cls
+    return decorator
+
+def create_simple_cases(func_name):
+    def decorator(cls):
+        func = getattr(cls, func_name)
+        casesFiles = glob.glob(simpleCasesPath)
+        # Set new funcs to class:
+        for file in casesFiles:
+            with open(file, 'r') as fh:
+                data = json.loads(fh.read())['cases']
+                for i, params in enumerate(data):
+                    def tmp(params=params):  # need for http://stackoverflow.com/q/7546285/1113207
+                        def wrapper(self, *args, **kwargs):
+                            return func(self, **params)
+                        return wrapper
+                    setattr(cls, func_name + '_' + file.replace('\\','/').split('/')[1].split('.')[0] + '_' + params["name"], tmp())
+            # Remove func from class:
+            setattr(cls, func_name, None)
         return cls
     return decorator
--- tests/test.py
+++ tests/test.py
@@ -7,15 +7,15 @@ import os
 import threading
 import unittest
 import tempfile
-from helper import create_tests, cases_from_files
+from helper import create_tests, cases_from_files, create_simple_cases
 
 interpreterPath = "../build/overstacked"
 inputCasePath = "/input.txt"
 outputCasePath = "/output.txt"
 
-@create_tests('test_operators', 'simpleCases.json')
-class SimpleTests(unittest.TestCase):
-    def test_operators(self, name, input, expectedOutput):
+@create_tests('test_old', 'simpleCases.json')
+class SimpleTestsOld(unittest.TestCase):
+    def test_old(self, name, input, expectedOutput):
         f = tempfile.NamedTemporaryFile(delete=False)
         f.write(input.encode("utf-8"))
         f.close()
@@ -39,6 +39,21 @@ class TestsFromFiles(unittest.TestCase):
         expectedOutput = expectedOutputFile.read()
         expectedOutputFile.close()
         assert output.decode("utf-8") == expectedOutput, f"{output.decode('utf-8')} != {expectedOutput}"
+
+@create_simple_cases('test')
+class SimpleTests(unittest.TestCase):
+    def test(self, name, input, expectedOutput):
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write(input.encode("utf-8"))
+        f.close()
+        inputFilePath = f.name
+        proc = sp.Popen([interpreterPath, inputFilePath], stdout=sp.PIPE)
+        (output, err) = proc.communicate()
+        proc.wait()
+        actualOutput = output.decode("utf-8").replace('\r','')
+        #print(f"[ {input} ] => {expectedOutput} ?")
+        assert repr(actualOutput) == repr(expectedOutput), f"{repr(actualOutput)} != {repr(expectedOutput)}"
+        os.unlink(f.name)
 
 
 if __name__ == "__main__":
