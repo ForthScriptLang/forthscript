81d6ebdcd40923f86c10e55e67698283f3c199f3
Iurii Zamiatin <umz38@yandex.ru>
2021-08-03 23:21:28 +0300

    repl: Fix error message

--- src/prelude/repl.cpp
+++ src/prelude/repl.cpp
@@ -3,6 +3,6 @@
 std::u32string getPreludeREPLSource() {
     return U"[get_stack_copy to_string \"\n# \" +] $_repl_prompt [True] "
            U"[_repl_prompt! readln parse get_stack_size 1 - inline_try not "
-           U"[\"error: \" swap + \"Clearing stack...\" + writeln] if] "
+           U"[\"error: \" swap + \". Clearing stack...\" + writeln] if] "
            U"inline_while";
 }
